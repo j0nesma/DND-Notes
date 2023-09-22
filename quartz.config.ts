872343cee6a46b6bd160ca34369bb5a2724a7972
Matthew Jones <matthewarjones@outlook.com>
2023-09-22 11:27:34 +0100

    init

--- quartz.config.ts
+++ quartz.config.ts
@@ -3,7 +3,7 @@ import * as Plugin from "./quartz/plugins"
 
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Campaign 2: Notes",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
